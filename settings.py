0761e9f3f18797275829f4331d3f242ebcace6f4
chqa <36105155@qq.com>
2023-07-08 22:36:12 +0800

    Update settings.py3

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
 
 # openAi 官方 api
 URL = "https://api.openai.com/v1/chat/completions"
